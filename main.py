bd26e295855dd90bd76fae1122b17aacc780ce9c
zxj <zengxj@szhorn.com>
2021-06-17 15:46:50 +0800

    按照爱心曲线绘制

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 def star():
-    for y in range(20, -20, -1):
-        for x in range(-20, 20, 1):
-            if x ** 2 + (y ** 2) * 4.14 <= 49:
+    for y in range(30, -30, -1):
+        for x in range(-30, 30, 1):
+            if ((x * 0.05) ** 2 + (y * 0.1) ** 2 - 1) ** 3 - (x * 0.05) ** 2 * (y * 0.1) ** 3 <= 0:
                 print('*', end='')
             else:
                 print(' ', end='')
