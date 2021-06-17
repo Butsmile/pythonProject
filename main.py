92ffed4c26ff420436198d936aae95c9860eeff6
zxj <zengxj@szhorn.com>
2021-06-17 15:30:47 +0800

    补正行间距后绘制的圆

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
 def star():
-    for x in range(-30, 30, 1):
-        for y in range(-30, 30, 1):
-            if x * 2 + y == 6:
-                print('+', end='')
+    for y in range(20, -20, -1):
+        for x in range(-20, 20, 1):
+            if x ** 2 + (y ** 2) * 4.14 <= 49:
+                print('*', end='')
             else:
-                print('-', end='')
+                print(' ', end='')
         print()
 
 #run field
